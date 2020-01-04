152ece0f74a4dfcc11e08c7df7face8aa05d89e6
Evan Soderberg <evansoderberg@gmail.com>
2020-01-03 17:33:42 -0800

    Updates

---
+++ precache-manifest.07139f9f32798cb562ee572e3d516b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e89c542dbcc6ed14155dae22c8e6c6",
+    "url": "/dataviz-builder/index.html"
+  },
+  {
+    "revision": "15cc8a068fcb1604b951",
+    "url": "/dataviz-builder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b80eb6d23458d906f8d9",
+    "url": "/dataviz-builder/static/js/2.514d2acf.chunk.js"
+  },
+  {
+    "revision": "712770cf6deb25e1dce7992a612b37a1",
+    "url": "/dataviz-builder/static/js/2.514d2acf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15cc8a068fcb1604b951",
+    "url": "/dataviz-builder/static/js/main.c744c837.chunk.js"
+  },
+  {
+    "revision": "b359e965f4d0064bbe68",
+    "url": "/dataviz-builder/static/js/runtime-main.63ca6b9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dataviz-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
