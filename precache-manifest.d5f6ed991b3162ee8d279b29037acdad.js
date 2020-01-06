8bde1d0cc267fcecd0c5bf6de997d89e039339f3
Evan Soderberg <evansoderberg@gmail.com>
2020-01-05 17:58:27 -0800

    Updates

--- precache-manifest.d5f6ed991b3162ee8d279b29037acdad.js
+++ precache-manifest.d5f6ed991b3162ee8d279b29037acdad.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbef13996f2ff9cc715c659e0063d0b6",
+    "revision": "fba36904ea2a7e0655f7265449786e08",
     "url": "/dataviz-builder/index.html"
   },
   {
-    "revision": "30470da5f19861d38dc2",
+    "revision": "ee431ef0c07a5f25b749",
     "url": "/dataviz-builder/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "77fa12e1504a7e3e1be6",
-    "url": "/dataviz-builder/static/js/2.4dae0458.chunk.js"
+    "revision": "1df3c5d1b7bcb8a55eb1",
+    "url": "/dataviz-builder/static/js/2.0a7d358a.chunk.js"
   },
   {
     "revision": "f3824db37b345bae14a058f2b51d7273",
-    "url": "/dataviz-builder/static/js/2.4dae0458.chunk.js.LICENSE"
+    "url": "/dataviz-builder/static/js/2.0a7d358a.chunk.js.LICENSE"
   },
   {
-    "revision": "30470da5f19861d38dc2",
-    "url": "/dataviz-builder/static/js/main.d648b9e1.chunk.js"
+    "revision": "ee431ef0c07a5f25b749",
+    "url": "/dataviz-builder/static/js/main.ea63c5e5.chunk.js"
   },
   {
     "revision": "b359e965f4d0064bbe68",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dataviz-builder/precache-manifest.8599678ab474fc00e0719a765d0b5abf.js"
+  "/dataviz-builder/precache-manifest.d5f6ed991b3162ee8d279b29037acdad.js"
 );
 
 self.addEventListener('message', (event) => {
