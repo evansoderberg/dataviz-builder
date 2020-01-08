25508ced56e5051daecc8bacfc769bb2862ba2e1
Evan Soderberg <evansoderberg@gmail.com>
2020-01-08 11:43:05 -0800

    Add link to sample data

--- src/components/csv/reader.jsx
+++ src/components/csv/reader.jsx
@@ -28,12 +28,22 @@ const Reader = props => {
   };
 
   return (
-    <CSVReader
-      cssClass="react-csv-input"
-      label="Select a CSV File"
-      onFileLoaded={onCSVData}
-      parserOptions={PARSER_OPTIONS}
-    />
+    <div>
+      <CSVReader
+        cssClass="react-csv-input"
+        label="Select a CSV File"
+        onFileLoaded={onCSVData}
+        parserOptions={PARSER_OPTIONS}
+      />
+      <a
+        className="sample-data"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/evansoderberg/dataviz-builder/tree/master/data"
+      >
+        Click here for sample data sources
+      </a>
+    </div>
   );
 };
 
